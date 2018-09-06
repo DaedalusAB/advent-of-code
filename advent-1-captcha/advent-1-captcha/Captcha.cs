2091e6306fe9db07af797d4ab30c9bb54b8ab193
Aleksandar Budić <aleksandar.budic@nultien.rs>
2018-09-06 14:16:40 +0200

    solution

--- advent-1-captcha/advent-1-captcha/Captcha.cs
+++ advent-1-captcha/advent-1-captcha/Captcha.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace advent_1_captcha
 {
     public class Captcha
     {
+        public static int SolveFor(int[] input)
+        {
+            var result = 0;
+            for (var i = 0; i < input.Length; i++)
+            {
+                var follower = (i + 1) % input.Length;
+                if (input[follower] == input[i])
+                {
+                    result += input[i];
+                }
+            }
+
+            return result;
+        }
     }
 }
